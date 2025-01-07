4a64f08555722fb082897d83edaf1835bf312eab
BenceGattyan <bencegattyan@gmail.com>
2025-01-07 13:28:21 +0100

    Testing ability to commit

--- src/test/java/org/acme/GreetingResourceIT.java
+++ src/test/java/org/acme/GreetingResourceIT.java
@@ -6,3 +6,5 @@ import io.quarkus.test.junit.QuarkusIntegrationTest;
 class GreetingResourceIT extends GreetingResourceTest {
     // Execute the same tests but in packaged mode.
 }
+
+// some new comment
